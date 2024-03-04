b62d5aaa7c769ed135c80f5ef40abf4867d022b3
Wesley Castilho <47651529+WesleyGCO@users.noreply.github.com>
2024-03-04 20:51:19 -0300

    Adicionado template

--- ProManage/paginas/urls.py
+++ ProManage/paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     #path("rota/", suaView, name="pagina-inicial")
     path("inicio/", IndexView.as_view(), name = 'index'),
+    path("sobre/", SobreView.as_view(), name = "sobre"),
 ]
--- ProManage/paginas/views.py
+++ ProManage/paginas/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView): # class Index extends TemplateView (herança)
     template_name = "paginas/index.html"
+
+
+class SobreView(TemplateView):
+    template_name = "paginas/sobre.html"
